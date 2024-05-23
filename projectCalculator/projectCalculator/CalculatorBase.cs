98eee0f13a526ef4269c3af19c16459f39d9a6ed
Anu S Nair <anusnair@Anus-MacBook-Pro-2.local>
2024-05-24 00:09:26 +1200

    Week_1 Assignment

---
+++ projectCalculator/projectCalculator/CalculatorBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace projectCalculator
+{
+	public abstract class CalculatorBase
+	{
+        public virtual int add(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine("parent");
+            return sum;
+        }
+        public virtual int sub(int a, int b)
+        {
+            int dif = a - b;
+            Console.WriteLine("parent");
+            return dif;
+        }
+        public virtual int mul(int a, int b)
+        {
+            int prdct = a * b;
+            Console.WriteLine("parent");
+            return prdct;
+        }
+     
+	}
+}
+
