fb5fe25e8513117d6bca028524e299859ee4a697
Anu S Nair <anusnair@Anus-MacBook-Pro-2.local>
2024-05-24 00:05:40 +1200

    Week_1 Assignment

---
+++ projectCalculator/projectCalculator/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace projectCalculator
+{
+	public class Calculator:CalculatorBase
+	{
+        public override int add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+        public override int sub(int a, int b)
+        {
+            int dif = a - b;
+            return dif;
+        }
+        public override int mul(int a, int b)
+        {
+            int prdct = a * b;
+            return prdct;
+        }
+    }
+}
+
